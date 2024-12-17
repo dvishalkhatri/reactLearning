6f34605807b2f4cb2d149f635b1dda076481dc9b
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-17 15:48:04 +0530

    Project Structure

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,9 +1,16 @@
 import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
 import Final from './tutorial/02-useEffect/final/05-cleanup-function';
+import Example from './tutorial/04-project-structure/starter/Example';
+// import Navbar from './tutorial/04-project-structure/starter/Navbar';
+// import { Home, About } from './tutorial/04-project-structure/starter/Pages';
 
 function App() {
   return (
     <div className='container'>
+      {/* <Navbar /> */}
+      {/* <Home /> */}
+      {/* <About /> */}
+      <Example />
       <Starter />
       {/* <Final /> */}
     </div>
