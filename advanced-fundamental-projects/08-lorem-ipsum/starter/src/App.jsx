b774d90d321986465b4875ebaf582c163ca4467a
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-19 16:58:32 +0530

    Finished with lorem ipsum Project

--- advanced-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
+++ advanced-fundamental-projects/08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,44 @@
+import { useState } from 'react';
+import text from './data';
+import { nanoid } from 'nanoid';
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [counter, setCounter] = useState(1);
+  const [textArray, setTextArray] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTextArray(text.slice(0, parseInt(counter)));
+    // console.log(textArray);
+  };
+
+  return (
+    <main>
+      <section className='section-center'>
+        <h4>TIRED OF BORING LOREM IPSUM?</h4>
+        <form action='' className='lorem-form' onSubmit={handleSubmit}>
+          <label htmlFor='lorem'>Paragraphs:</label>
+          <input
+            type='number'
+            name='lorem'
+            id='lorem'
+            min='1'
+            max='8'
+            step='1'
+            value={counter}
+            onChange={(e) => setCounter(e.target.value)}
+          />
+          <button type='submit' className='btn'>
+            Generate
+          </button>
+        </form>
+        <article className='lorem-text'>
+          {textArray.map((text) => {
+            return <p key={nanoid()}>{text}</p>;
+          })}
+        </article>
+      </section>
+    </main>
+  );
 };
 export default App;
