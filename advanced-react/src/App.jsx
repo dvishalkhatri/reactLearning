6ef8023429548a2b4dfd66d16d69c2d6a000e872
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-23 12:30:30 +0530

    Finished with useRef Hook

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/06-forms/starter/05-form-data';
-import Final from './tutorial/06-forms/final/05-form-data';
+import Starter from './tutorial/07-useRef/starter/01-useRef-basics';
+import Final from './tutorial/07-useRef/final/01-useRef-basics';
 // import Example from './tutorial/04-project-structure/starter/Example';
 // import Navbar from './tutorial/04-project-structure/starter/Navbar';
 // import { Home, About } from './tutorial/04-project-structure/starter/Pages';
