6124bc90b55178a8f425496655cf74e818bde6e8
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-23 16:06:17 +0530

    Finished with Custom Hooks

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/07-useRef/starter/01-useRef-basics';
-import Final from './tutorial/07-useRef/final/01-useRef-basics';
+import Starter from './tutorial/08-custom-hooks/starter/02-fetch-data';
+import Final from './tutorial/08-custom-hooks/final/02-fetch-data';
 // import Example from './tutorial/04-project-structure/starter/Example';
 // import Navbar from './tutorial/04-project-structure/starter/Navbar';
 // import { Home, About } from './tutorial/04-project-structure/starter/Pages';
