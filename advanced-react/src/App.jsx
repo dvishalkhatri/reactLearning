bc678fc34e3e35ea5a46f398c8ef0d5de39309b2
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-24 17:25:11 +0530

    Finished with useReducer

--- advanced-react/src/App.jsx
+++ advanced-react/src/App.jsx
@@ -1,5 +1,5 @@
-import Starter from './tutorial/09-context-api/starter/context/Navbar';
-import Final from './tutorial/09-context-api/final/context/Navbar';
+import Starter from './tutorial/10-useReducer/starter/01-useReducer';
+import Final from './tutorial/10-useReducer/final/01-useReducer';
 // import Example from './tutorial/04-project-structure/starter/Example';
 // import Navbar from './tutorial/04-project-structure/starter/Navbar';
 // import { Home, About } from './tutorial/04-project-structure/starter/Pages';
--- advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import { useReducer, useState } from 'react';
 import { data } from '../../../data';
+import { CLEAR_LIST, RESET_LIST, REMOVE_ITEM } from './action';
+import reducer from './reducer';
+
+const defaultState = {
+  people: data,
+  isLoading: false,
+};
+
 const ReducerBasics = () => {
-  const [people, setPeople] = React.useState(data);
+  const [state, dispatch] = useReducer(reducer, defaultState);
+  // const [people, setPeople] = useState(data);
 
   const removeItem = (id) => {
-    let newPeople = people.filter((person) => person.id !== id);
-    setPeople(newPeople);
+    dispatch({ type: REMOVE_ITEM, payload: { id } });
+    // let newPeople = people.filter((person) => person.id !== id);
+    // setPeople(newPeople);
   };
+  const clearList = () => {
+    dispatch({ type: CLEAR_LIST });
+    // setPeople([]);
+  };
+  const resetList = () => {
+    dispatch({ type: RESET_LIST });
+    // setPeople(data);
+  };
+  console.log(state);
   return (
     <div>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
           <div key={id} className='item'>
@@ -18,13 +37,24 @@ const ReducerBasics = () => {
           </div>
         );
       })}
-      <button
-        className='btn'
-        style={{ marginTop: '2rem' }}
-        onClick={() => setPeople([])}
-      >
-        clear items
-      </button>
+      {state.people.length < 1 ? (
+        <button
+          type='button'
+          className='btn'
+          style={{ marginTop: '2rem' }}
+          onClick={resetList}
+        >
+          Reset
+        </button>
+      ) : (
+        <button
+          className='btn'
+          style={{ marginTop: '2rem' }}
+          onClick={clearList}
+        >
+          clear items
+        </button>
+      )}
     </div>
   );
 };
