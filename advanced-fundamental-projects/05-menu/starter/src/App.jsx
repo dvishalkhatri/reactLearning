c182333f2bfbf9b182f8dbadf27f0b52b9258be0
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2024-12-31 16:53:47 +0530

    Finished with Menu Project

--- advanced-fundamental-projects/05-menu/starter/src/App.jsx
+++ advanced-fundamental-projects/05-menu/starter/src/App.jsx
@@ -1,4 +1,30 @@
+import { useState } from 'react';
+import Title from './Title';
+import items from './data';
+import Menu from './Menu';
+import Categories from './Categories';
+const categories = ['all', ...new Set(items.map((item) => item.category))];
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const filterItems = (category) => {
+    setMenuItems(items);
+    if (category === 'all') {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = menuItems.filter((menu) => menu.category === category);
+    setMenuItems(newItems);
+    return;
+  };
+  return (
+    <main>
+      <section className='menu'>
+        <Title text='our menu' />
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 };
 export default App;
