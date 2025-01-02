5faf34cc5468f93661ff9f85e669664a290eb7cb
Vishal Kumar Khatri <dvishalkhatri@gmail.com>
2025-01-02 17:10:04 +0530

    Finished with Tabs Project

--- advanced-fundamental-projects/06-tabs/starter/src/App.jsx
+++ advanced-fundamental-projects/06-tabs/starter/src/App.jsx
@@ -1,6 +1,62 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import Loading from './Loading';
+import JobInfo from './JobInfo';
+import BtnContainer from './BtnContainer';
+
 const url = 'https://www.course-api.com/react-tabs-project';
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [currentItem, setCurrentItem] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        setError(true);
+        return;
+      }
+      const res = await response.json();
+      setJobs(res);
+    } catch (error) {
+      console.log(error);
+      setError(true);
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (error) {
+    return (
+      <section className='jobs-center'>
+        <h1>Sorry, there was error in fetching the data</h1>
+      </section>
+    );
+  }
+
+  if (isLoading) {
+    return (
+      <section className='jobs-center'>
+        <Loading />
+      </section>
+    );
+  }
+
+  return (
+    <section className='jobs-center'>
+      <BtnContainer
+        jobs={jobs}
+        currentItem={currentItem}
+        setCurrentItem={setCurrentItem}
+      />
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
 };
 export default App;
---
+++ advanced-fundamental-projects/06-tabs/starter/src/BtnContainer.jsx
@@ -0,0 +1,19 @@
+const BtnContainer = ({ jobs, currentItem, setCurrentItem }) => {
+  return (
+    <div className='btn-container'>
+      {jobs.map((item, index) => {
+        return (
+          <button
+            type='button'
+            className={index === currentItem ? 'job-btn active-btn' : 'job-btn'}
+            key={item.id}
+            onClick={() => setCurrentItem(index)}
+          >
+            {item.company}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
+export default BtnContainer;
